6f306abba6f6d4b20d9d41d89b1acc87a9974e5f
ZJUFENGJIe <3170105453@zju.edu.cn>
2019-12-09 14:52:20 +0800

    esay one but have to think about time efficiency

---
+++ harvest.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <cstring>
+#include<algorithm>
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+    int n, m;
+    std::iostream::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    int app[100000] = { 0 };
+    for (int i = 0; i < n; i++)
+    {
+        cin >> app[i];
+         
+    }
+    cin >> m;
+    vector<pair<int, int>> vec(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >>vec[i].first;
+        vec[i].second = i;
+    }
+    for (int i = 1; i < n; i++)
+    {
+        app[i] += app[i - 1];
+    }
+    sort(vec.begin(), vec.end());
+    int j = 0;
+    vector<int> display(m);
+    for (int i = 0; i<m; i++)
+    {
+        while (vec[i].first>app[j])
+            j++;
+        display[vec[i].second] = j+1;
+    }
+    for (int i = 0; i<m; i++)
+        printf("%d\n",display[i] );
+    //二分查找！
+    /*cin >> m;
+    for (int i = 0; i != m; ++i) {
+        cin >> q;
+        cout<< lower_bound(appsum.begin(), appsum.end(), make_pair(q,0))->second<< endl;
+    }*/
+    return 0;
+}
